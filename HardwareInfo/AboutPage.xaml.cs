21fce3a551634fac6dbd5d31db00f4f1261fc71c
Tomi Paananen <tomi.paananen@nokia.com>
2013-12-11 14:06:04 +0200

    Forgot to add the files.

---
+++ HardwareInfo/AboutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using HardwareInfo.Resources;
+using System.Xml.Linq;
+
+namespace HardwareInfo
+{
+    public partial class AboutPage : PhoneApplicationPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+            VersionTextBlock.Text = AppResources.Version + " " + XDocument.Load("WMAppManifest.xml").Root.Element("App").Attribute("Version").Value;
+        }
+    }
+}
